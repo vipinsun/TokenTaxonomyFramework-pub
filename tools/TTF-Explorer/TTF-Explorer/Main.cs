121c00b3eff8defedd1518254cd03bc21e031256
Marley Gray <newgate-marleyg@hotmail.com>
2019-05-23 18:11:46 -0700

    TTF-Explorer
    Xamarin OSX TTF Explorer Client

---
+++ tools/TTF-Explorer/TTF-Explorer/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace TTFExplorer
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
