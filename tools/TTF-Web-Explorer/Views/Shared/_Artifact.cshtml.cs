8a142876232e3108bf9472815749d21d50366914
Marley Gray <marleyg@microsoft.com>
2019-06-04 16:54:40 -0400

    Break

--- tools/TTF-Web-Explorer/Views/Shared/_Artifact.cshtml.cs
+++ tools/TTF-Web-Explorer/Views/Shared/_Artifact.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TTI.TTF.WebExplorer.Views.Shared
 {
-	public class artifact : PageModel
+	public class artifact : Page
 	{
 		public void OnGet()
 		{
