0a18daec809355b0a81a290c3007f27d5456404f
Antoine Toulme <antoine@lunar-ocean.com>
2019-08-02 22:00:54 -0700

    Add envoy proxy

--- tools/TTF-Web-UI/src/index.ts
+++ tools/TTF-Web-UI/src/index.ts
@@ -3,7 +3,7 @@ import {ServiceClient} from '../../TaxonomyObjectModel/out/ts/ServiceServiceClie
 import {ConfigurationRequest} from "../../TaxonomyObjectModel/out/ts/artifact_pb";
 import * as artifact_pb from "../../TaxonomyObjectModel/out/ts/artifact_pb";
 
-let client = new ServiceClient("http://localhost:8086");
+let client = new ServiceClient("http://localhost:9080");
 
 let call = client.getConfig(new ConfigurationRequest(), null, (err: grpcWeb.Error,
                                                                              response: artifact_pb.ServiceConfiguration) => {
--- tools/TaxonomyService/docker-build.sh
+++ tools/TaxonomyService/docker-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 docker build -f TaxonomyClient/Dockerfile --rm -t txclient .
 docker build -f TaxonomyService/Dockerfile --rm -t tti/ttf/taxonomyservice .
+docker build -f envoy/Dockerfile --rm -t tti/ttf/envoy .
 docker-compose up 
