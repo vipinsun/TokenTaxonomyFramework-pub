89a495eccf5efc2e618b3073fce1635ee43f172a
Antoine Toulme <antoine@lunar-ocean.com>
2020-01-16 18:06:15 -0800

    Move the editor mapping to the render function so data has loaded by the time the editor shows

--- tools/TTF-Web-UI/src/pages/editor.tsx
+++ tools/TTF-Web-UI/src/pages/editor.tsx
@@ -24,7 +24,8 @@ const formItemLayout = {
 };
 
 interface BaseFormProps extends FormComponentProps {
-  selected: Artifact,
+  state: IStoreState,
+  currentEntity: string
 }
 
 class BaseForm extends React.Component<BaseFormProps, any> {
@@ -32,147 +33,157 @@ class BaseForm extends React.Component<BaseFormProps, any> {
   public render() {
     const {getFieldDecorator} = this.props.form;
     const form: WrappedFormUtils = this.props.form;
-    const formName = this.props.selected.getName();
+    const state = this.props.state;
+    const currentID = this.props.currentEntity;
+    let selected = state.entities.bases.byId.get(currentID) ||
+      state.entities.behaviors.byId.get(currentID) ||
+      state.entities.behaviorGroups.byId.get(currentID) ||
+      state.entities.propertySets.byId.get(currentID) ||
+      state.entities.templateDefinitions.byId.get(currentID);
+    if (selected === undefined) {
+      return <div/>
+    } else {
+      const formName = selected.getName();
 
-    return <div className={"form"}>
-      <Form {...formItemLayout}>
-        <h2>{formName}</h2>
-        <div className="form-wrapper">
-          <h3 dangerouslySetInnerHTML={{__html:"Edit " +
-              // @ts-ignore
-              this.props.selected.getArtifactSymbol().getVisual()}}/>
-          <div className="inputs-wrapper">
-            <Form.Item label="Name">
-              {getFieldDecorator('name', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input a token name',
-                  },
-                ],
-              })(<Input disabled={!editable}/>)}
-            </Form.Item>
-            <Form.Item label="Symbol">
-              {getFieldDecorator('symbol', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input a token symbol',
-                  },
-                ],
-              })(<Input disabled={!editable}/>)}
-            </Form.Item>
-            <Form.Item label="Owner">
-              {getFieldDecorator('owner', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input a token owner',
-                  },
-                ],
-              })(<Input disabled={!editable}/>)}
-            </Form.Item>
-            <Form.Item label="Quantity">
-              {getFieldDecorator('quantity', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please enter a quantity',
-                  },
-                ],
-              })(<Input disabled={!editable}/>)}
-            </Form.Item>
-            <Form.Item label="Decimals">
-              {getFieldDecorator('decimals', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please enter the number of decimals allowed',
-                  },
-                ],
-              })(<Input disabled={!editable}/>)}
-            </Form.Item>
-            <Form.Item label="Constructor name">
-              {getFieldDecorator('constructor_name', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please enter the name of the constructor',
-                  },
-                ],
-              })(<Input disabled={!editable}/>)}
-            </Form.Item>
-          </div>
-          <div className="add-wrapper">
-            <Form.Item
-              wrapperCol={{
-                xs: {span: 24, offset: 8},
-                sm: {span: 20, offset: 5},
-              }}
-            >
-              {new KeyValue({field: "token_properties", form: form, label: "Token properties", disabled: !editable}).render()}
-            </Form.Item>
-          </div>
-          {editable ?
-            <div className="submit-wrapper">
+      return <div className={"form"}>
+        <Form {...formItemLayout}>
+          <h2>{formName}</h2>
+          <div className="form-wrapper">
+            <h3 dangerouslySetInnerHTML={{
+              __html: "Edit " +
+                // @ts-ignore
+                selected.getArtifactSymbol().getVisual()
+            }}/>
+            <div className="inputs-wrapper">
+              <Form.Item label="Name">
+                {getFieldDecorator('name', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input a token name',
+                    },
+                  ],
+                })(<Input disabled={!editable}/>)}
+              </Form.Item>
+              <Form.Item label="Symbol">
+                {getFieldDecorator('symbol', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input a token symbol',
+                    },
+                  ],
+                })(<Input disabled={!editable}/>)}
+              </Form.Item>
+              <Form.Item label="Owner">
+                {getFieldDecorator('owner', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input a token owner',
+                    },
+                  ],
+                })(<Input disabled={!editable}/>)}
+              </Form.Item>
+              <Form.Item label="Quantity">
+                {getFieldDecorator('quantity', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please enter a quantity',
+                    },
+                  ],
+                })(<Input disabled={!editable}/>)}
+              </Form.Item>
+              <Form.Item label="Decimals">
+                {getFieldDecorator('decimals', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please enter the number of decimals allowed',
+                    },
+                  ],
+                })(<Input disabled={!editable}/>)}
+              </Form.Item>
+              <Form.Item label="Constructor name">
+                {getFieldDecorator('constructor_name', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please enter the name of the constructor',
+                    },
+                  ],
+                })(<Input disabled={!editable}/>)}
+              </Form.Item>
+            </div>
+            <div className="add-wrapper">
               <Form.Item
                 wrapperCol={{
-                  xs: {span: 24, offset: 0},
-                  sm: {span: 16, offset: 8},
+                  xs: {span: 24, offset: 8},
+                  sm: {span: 20, offset: 5},
                 }}
               >
-                <Button type="primary" htmlType="submit" className="submit">Submit</Button>
+                {new KeyValue({
+                  field: "token_properties",
+                  form: form,
+                  label: "Token properties",
+                  disabled: !editable
+                }).render()}
               </Form.Item>
-            </div>: null}
-        </div>
-      </Form>
-    </div>
+            </div>
+            {editable ?
+              <div className="submit-wrapper">
+                <Form.Item
+                  wrapperCol={{
+                    xs: {span: 24, offset: 0},
+                    sm: {span: 16, offset: 8},
+                  }}
+                >
+                  <Button type="primary" htmlType="submit" className="submit">Submit</Button>
+                </Form.Item>
+              </div> : null}
+          </div>
+        </Form>
+      </div>
+    }
   }
 };
 
 const Editor = Form.create<BaseFormProps>({
   mapPropsToFields: (props: BaseFormProps) => {
-    // @ts-ignore
-    const symbol = props.selected.getArtifactSymbol().getVisual();
-    return {name: Form.createFormField({
-        name: props.selected.getName(),
-        value: props.selected.getName(),
-      }),
-      symbol: Form.createFormField({
-        name: symbol,
-        value: symbol,
-      })};
+    const state = props.state;
+    const currentID = props.currentEntity;
+    let selected = state.entities.bases.byId.get(currentID) ||
+      state.entities.behaviors.byId.get(currentID) ||
+      state.entities.behaviorGroups.byId.get(currentID) ||
+      state.entities.propertySets.byId.get(currentID) ||
+      state.entities.templateDefinitions.byId.get(currentID);
+    if (selected === undefined) {
+      return {};
+    } else {
+      // @ts-ignore
+      const symbol = selected.getArtifactSymbol().getVisual();
+      return {
+        name: Form.createFormField({
+          name: selected.getName(),
+          value: selected.getName(),
+        }),
+        symbol: Form.createFormField({
+          name: symbol,
+          value: symbol,
+        })
+      };
+    }
   }
 })(BaseForm);
 
 export default connect(
   (state: IStoreState) => {
-    // @ts-ignore
-    const currentLocation = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
-    // @ts-ignore
-    let selected = state.entities.bases.byId.get(currentLocation);
-    if (selected === undefined) {
-      // @ts-ignore
-      selected = state.entities.behaviors.byId.get(currentLocation);
-    }
-    if (selected === undefined) {
-      // @ts-ignore
-      selected = state.entities.behaviorGroups.byId.get(currentLocation);
-    }
-    if (selected === undefined) {
-      // @ts-ignore
-      selected = state.entities.propertySets.byId.get(currentLocation);
-    }
-    if (selected === undefined) {
-      // @ts-ignore
-      selected = state.entities.templateDefinitions.byId.get(currentLocation);
-    }
+    const currentID = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
 
-    if (selected === undefined) {
-      throw "should not be there";
-    }
     return {
-      selected: selected
+      currentEntity: currentID,
+      state: state
     }
   },
   { },
